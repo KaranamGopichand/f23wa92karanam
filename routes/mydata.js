b3ff11d6a368d988f6e2266e4e4e925a40669da5
KaranamGopichand <srigopichand2000@gmail.com>
2023-10-25 21:08:15 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Karanam Gopichand' });
+  res.render('mydata', { title: 'Karanam Gopichand' });
 });
 
 module.exports = router;
