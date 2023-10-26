24d8b80cf764fb4f26f3876a74f1c8c0a5c90971
KaranamGopichand <srigopichand2000@gmail.com>
2023-10-25 20:56:01 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-// router.get('/',function(req,res,next){
-// res.send('respond with a resource');
-// });
+let sum = 0;
+let more = 0;
 
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  var sum = 0;
-  var more = 0;
-  more = more + 1;
-  sum+= more;
-  res.send(`Sum is : ` +sum);
+  more++
+  sum+=more;
+  res.send('Sum is: '+ sum );
 });
 
 module.exports = router;
